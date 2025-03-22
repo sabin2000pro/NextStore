59ecab543966c295896376585149f4a33c24a654
sabin2000pro <sabinlungu293@gmail.com>
2025-03-22 16:16:50 +0000

    Adding skeleton authentication API controllers

--- backend/controllers/authentication-controllers.js
+++ backend/controllers/authentication-controllers.js
@@ -1,4 +1,38 @@
+const asyncHandler = require('express-async-handler');
+
+// @description: Registers a new user account.
+// @method: POST
+// @route: POST /api/v1/auth/register
+// @returns: 201 - HTTP CREATED
 
 module.exports.registerUser = async (request, response, next) => {
 
 }
+
+// @description: Logs the user out from the application.
+// @method: GET
+// @route: /api/v1/auth/logout
+// @returns: 200 - HTTP OK
+
+module.exports.loginUser = async (request, response, next) => {
+
+}
+
+// @description: Logs the user out from the application.
+// @method: GET
+// @route: /api/v1/auth/logout
+// @returns: 200 - HTTP OK
+
+module.exports.forgotPassword = async (request, response, next) => {
+
+}
+
+// @description: Logs the user out from the application.
+// @method: GET
+// @route: /api/v1/auth/logout
+// @returns: 200 - HTTP OK
+
+module.exports.logout = async (request, response, next) => {
+
+}
+
---
+++ backend/database/connect-db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const DB_URI = process.env.DB_URI || ""
+
+module.exports.connectDatabase = async () => {
+    try {
+    await mongoose.connect(DB_URI).then((conn) => {
+            if(conn.connection) {
+                console.log('Connected to database successfully...')
+            }
+
+         })
+    } 
+    
+    catch(error) {
+
+        if(error) {
+            console.log('Error connecting to database...', error?.message)
+        }
+
+    }
+
+}
